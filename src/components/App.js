fb1ad1b539005d34ed7f34e8ff7540af57e58d66
Sergey Mezhmal <s.mezhmal@gmail.com>
2020-10-26 01:28:17 +0300

    init

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="page-header">
+        <h1>COVID-19</h1>
+      </div>
+      <div className="row">
+        <div className="col-md-6">
+          column 1
+        </div>
+        <div className="col-md-6">
+          column 2
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
